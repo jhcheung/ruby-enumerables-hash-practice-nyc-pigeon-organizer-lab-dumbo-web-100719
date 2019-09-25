00da2f1ec2543b665caa9652bf03e3fa8bf9c08b
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-09-25 05:18:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,9 @@
 def nyc_pigeon_organizer(data)
   data.reduce({}) do |memo, (quality, value)|
     value.each do |detail, value2|
-      
+      value2.times do |count|
+        if 
+      end 
     end 
   end 
 end
