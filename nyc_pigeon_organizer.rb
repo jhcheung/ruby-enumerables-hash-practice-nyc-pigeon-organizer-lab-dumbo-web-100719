9b5cd09dffa5ea2fc99465df9a940cb646d0a92b
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-09-25 05:26:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,10 @@ def nyc_pigeon_organizer(data)
       value2.times do |count|
         if !memo[value2[count]]
           memo[value2[count]] = {}
-          memo[value2[count]][:quality] = []
-          
+          memo[value2[count]][quality] = []
+          memo[value2[count]][quality].push(detail.to_s)
+        else 
+          memo[value2[count]][quality].push(detail.to_s)
         end 
       end 
     end 
